24c3cabad420c99ed3f56d36bcc091acf53f5dd3
ayeagle <alexyeagle@gmail.com>
2023-02-05 11:11:19 -0800

    init

--- pages/authorize.js
+++ pages/authorize.js
@@ -2,6 +2,27 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'PATCH',
+  url: 'https://dev-r2zpy4nl1f3sy1k8.us.auth0.com/api/v2/clients/ewTvhE2obV76Nkxv89NxX99OpQBTe9Fu',
+  headers: {
+    'content-type': 'application/json',
+    authorization: 'Bearer API2_ACCESS_TOKEN',
+    'cache-control': 'no-cache'
+  },
+  data: {initiate_login_uri: '<login_url>'}
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+
 export default function Home() {
   return (
     <div className="container">
