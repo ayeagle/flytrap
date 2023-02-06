6c0e1edc97bbf6e4bcce2db91df31b1eea7ed0d8
ayeagle <alexyeagle@gmail.com>
2023-02-06 15:15:40 -0800

    npm updates

--- pages/authorize.js
+++ pages/authorize.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
+import axios from 'axios';
 
 var axios = require("axios").default;
 
