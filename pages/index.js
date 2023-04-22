b4403db2f4d7ff30accfa566b47861f250b080f0
ayeagle <alexyeagle@gmail.com>
2023-04-21 19:21:17 -0700

    flytrap formatting

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import LandingPage from '@components/LandingPage'
-import styles from './index.module.css'
-import Screenshots from "@components/Screenshots"
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import LandingPage from "@components/LandingPage";
+import styles from "./index.module.css";
+import Screenshots from "@components/Screenshots";
 
 //stuff
 //dsd
 //dsjdhsj
-
 
 export default function Home() {
   return (
@@ -19,16 +18,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.master_container}>
-        <img src='Media/logos/logo3.png' style={{ width: "15vw", height: "auto" }} />
-        <h1>Flytrap </h1>
-        <h3>The all-in-one extension to make email signups easier, safer, and more accountable.</h3>
-        <Screenshots />
+        <img
+          src="Media/logos/logo3.png"
+          style={{ width: "15vw", height: "auto" }}
+        />
+        <div className={styles.typing_animate}>
 
-        <button className={styles.chrome_button}>Download on Chrome Store</button>        <p className="description">
-        </p>
+           <h1 >
+            Flytrap
+          </h1>
+        <h3>
+          The all-in-one extension to make email signups easier, safer, and more
+          accountable.
+        </h3>
+        </div>
+
+        <Screenshots />
+       
+        <button className={styles.chrome_button}>
+          Download on Chrome Store
+        </button>{" "}
+        <p className="description"></p>
       </main>
 
       {/* <Footer /> */}
     </div>
-  )
+  );
 }
